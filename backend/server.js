9a053739d2626fef6446283ddeccd16d23f8072c
Auriane SE <aurianesenou@gmail.com>
2026-04-01 12:37:30 +0200

    Add Vrcel URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const PORT = process.env.PORT || 3001;
 
 //Middlewares globaux 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://plateforme-rh-one.vercel.app/"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
